862c456972db54ecb4fce40cafec8d83e47d23e9
SeongWooHong <pluto0303@naver.com>
2023-09-04 20:18:07 +0900

    Add LoginActivity

---
+++ app/src/main/java/com/example/apple_tree/LoginActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apple_tree
+
+class LoginActivity {
+    
+}
---
+++ app/src/main/java/com/example/apple_tree/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.apple_tree.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
